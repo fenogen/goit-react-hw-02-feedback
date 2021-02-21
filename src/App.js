850f361d607cadda6d88ff4159a964eea6dea471
Oleg Soldatenko <sunskays@gmail.com>
2021-02-21 18:36:25 +0200

    Функция Total and percent

--- src/App.js
+++ src/App.js
@@ -9,43 +9,51 @@ state = {
   good: 0,
   neutral: 0,
   bad: 0,
-  total: 0,
-  positivePersent: 0,
+  total: 1,
+  positivePerсent: 0,
 }
   
-  plus = (ev) => {
+   fnPlus = (ev) => {
 
     if (ev.target.id === "plus") {
-      this.setState({ good: this.state.good + 1 })
+      this.setState((prevState) =>
+        ({ good: prevState.good + 1 }))
     }
+     
     if (ev.target.id === "neutral") {
       this.setState({ neutral: this.state.neutral + 1 })
     }
     if (ev.target.id === "bad") {
       this.setState({ bad: this.state.bad + 1 })
     }
+     
+     this.fnTotal();
+     this.fnPositivePerсent();
   }
 
-  total = () => {
-    
+   fnTotal = () => {
+    this.setState({ total: this.state.good + this.state.neutral + this.state.bad})
+   }
+  
+  fnPositivePerсent = () => {
+    this.setState({ positivePerсent: this.state.good * 100 / this.state.total })
   }
-
 
   render() {
     return (
       <div className="">
         <h1>Please leave feedback</h1>
         <div>
-          <button id="plus" onClick={this.plus}>Good</button>
-          <button id="neutral" onClick={this.plus}>Neutral</button>
-          <button id="bad" onClick={this.plus}>Bad</button>
+          <button id="plus" onClick={this.fnPlus}>Good</button>
+          <button id="neutral" onClick={this.fnPlus}>Neutral</button>
+          <button id="bad" onClick={this.fnPlus}>Bad</button>
         </div>
         <ul> Statistics
           <li>Good: {this.state.good}</li>
           <li>Neutral: {this.state.neutral}</li>
           <li>Bad: {this.state.bad}</li>
           <li>Total: {this.state.total}</li>
-          <li>Positive feedback: {this.state.positivePersent} %</li>
+          <li>Positive feedback: {this.state.positivePerсent} %</li>
           </ul>
       </div>
     )
