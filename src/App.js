bdd208c3432f8dcea383b2adbadfda167c30ec14
Oleg Soldatenko <sunskays@gmail.com>
2021-02-21 17:51:06 +0200

    Функция добавления

--- src/App.js
+++ src/App.js
@@ -1,25 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+// import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React, { Component } from 'react'
+import PropTypes from 'prop-types'
+
+export default class App extends Component {
+
+state = {
+  good: 0,
+  neutral: 0,
+  bad: 0,
+  total: 0,
+  positivePersent: 0,
+}
+  
+  plus = (ev) => {
+
+    if (ev.target.id === "plus") {
+      this.setState({ good: this.state.good + 1 })
+    }
+    if (ev.target.id === "neutral") {
+      this.setState({ neutral: this.state.neutral + 1 })
+    }
+    if (ev.target.id === "bad") {
+      this.setState({ bad: this.state.bad + 1 })
+    }
+  }
+
+  total = () => {
+    
+  }
+
+
+  render() {
+    return (
+      <div className="">
+        <h1>Please leave feedback</h1>
+        <div>
+          <button id="plus" onClick={this.plus}>Good</button>
+          <button id="neutral" onClick={this.plus}>Neutral</button>
+          <button id="bad" onClick={this.plus}>Bad</button>
+        </div>
+        <ul> Statistics
+          <li>Good: {this.state.good}</li>
+          <li>Neutral: {this.state.neutral}</li>
+          <li>Bad: {this.state.bad}</li>
+          <li>Total: {this.state.total}</li>
+          <li>Positive feedback: {this.state.positivePersent} %</li>
+          </ul>
+      </div>
+    )
+  }
 }
 
-export default App;
